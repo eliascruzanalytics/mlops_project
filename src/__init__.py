275955d5ec1a451db1a823e3456ee19d6e55a5f3
elias.cruz <elias.cruz@hotmail.com.br>
2026-05-18 21:25:42 -0300

    mlflow parte 3 b

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,14 @@
 import logging
+import dagshub
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# Initialize DagsHub with credentials
+dagshub.init(
+    repo_owner='eliascruzanalytics',
+    repo_name='mlops_project'
+)
 
 # Configure the logging strategy
 logging.basicConfig(
